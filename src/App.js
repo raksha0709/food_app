e11e616c92f0a4971a8ceece37720ceb1d8008b7
raksha0709 <rakshakunder9@gmail.com>
2023-09-06 17:58:47 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,75 @@
-import React from 'react';
-import './Components/style.css';
-import Meal from './Components/Meal';
-import Recipe from './Components/Recipe';
-import {Route,Routes} from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import SearchBar from "./Components/SearchBar";
+import RecipeCard from "./Components/RecipeCard";
+
+const searchApi = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
 function App() {
-  return (
-    <>
-   <Routes>
-      <Route  path="/" element={<Meal/>}/>
-      <Route exact path="/:recipeId" element={<Recipe/>}/>
-      </Routes>
-    </>
-  )
-}
+  const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // Add state for authentication
 
-export default App;
+  const searchRecipes = async () => {
+    setIsLoading(true);
+    const url = searchApi + query;
+    const res = await fetch(url);
+    const data = await res.json();
+    setRecipes(data.meals);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    searchRecipes();
+  }, [query]);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    searchRecipes();
+  };
+
+  const handleLogin = () => {
+    // In a real application, you'd implement actual login logic here (e.g., Firebase Authentication).
+    // For this example, we'll simulate a login after 2 seconds.
+    setTimeout(() => {
+      setIsLoggedIn(true);
+    }, 2000);
+  };
+
+  return (
+    <div className="container">
+      {isLoggedIn ? (
+        // If the user is logged in, show the home page
+        <>
+          <h2>Welcome to Our Food Recipes</h2>
+          <SearchBar
+            isLoading={isLoading}
+            query={query}
+            setQuery={setQuery}
+            handleSubmit={handleSubmit}
+          />
+          <div className="card-container">
+            {recipes ? (
+              recipes.map((recipe) => (
+                <RecipeCard
+                  key={recipe.idMeal}
+                  recipe={recipe}
+                  />
+                  ))
+                ) : (
+                  <p>No Results.</p>
+                )}
+              </div>
+            </>
+          ) : (
+            // If the user is not logged in, show the login page
+            <div className="login-container">
+              <h2>Login</h2>
+              <button onClick={handleLogin}>Login</button>
+            </div>
+          )}
+        </div>
+      );
+    }
+    
+    export default App;
